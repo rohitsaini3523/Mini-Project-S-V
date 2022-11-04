dd7e375ad7aa287d8ab018e394f231c5fa31d4ca
Rohit Saini <rohitsaini3523@gmail.com>
2022-11-04 11:39:31 +0530

    basic

---
+++ DBMS - SERVICE CENTER MANAGEMENT SYSTEM/SCM/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <!-- Bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body style="padding-top:5%;">
+    <center>
+        <div class="container" style="padding: 15%;">
+            <h1 style="font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif">School of Computers</h1>
+            <button type="button" class="btn" style="background-color:yellow;"><a href="regestration.php" style="text-decoration:none;color:red;">Sign up</a></button>
+            <button type="button" class="btn" style="background-color:lightblue;"><a href="login.php" style="text-decoration:none;color:black;">Sign in</a></button>
+        </div>
+    </center>
+</body>
+
+</html>
---
+++ DBMS - SERVICE CENTER MANAGEMENT SYSTEM/SCM/login.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+if (isset($_SESSION["email"]) && $_SESSION["pass"] === true) {
+    header("location: profile.php");
+    exit;
+}
+else
+{
+    $_SESSION["email"] = false;
+    $_SESSION["pass"] = false;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login </title>
+    <!-- Bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <center>
+        <h1 id="form"></h1>
+        <div class="form" style="width:60% ;">
+            <h1 style=" color:black; font-size: large;" id="heading">Login</h1>
+            <form name="myform" method="post" action="profile.php">
+                <div class="form_design" id="email">
+                    <input type="email" name="femail" id="" required placeholder="Enter E-mail here"><b><br><span class="form_error"> </span></b>
+                </div>
+                <div class="form_design" id="pass">
+                    <input type="password" name="fpass" id="" required placeholder="Password"><b><br><span class="form_error"></span></b>
+                </div>
+                <input type="submit" class="button" value="Login" id="submitbutton">
+                <h1 style=" color:black; font-size: large;" id="checker"><a href="regestration.php" style="color:black ;text-decoration:none;">Not Registered?</a></h1>
+    </center>
+    </div>
+    </div>
+    </form>
+    
+    <script src="algo.js">
+    </script>
+
+</body>
+
+</html>
